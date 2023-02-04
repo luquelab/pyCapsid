b55ffbc1812519c8606f6570e12fbda8d9127080
colintravisbrown <54756825+colintravisbrown@users.noreply.github.com>
2023-02-03 17:10:02 -0800

    Testing chimerax_script.py and viz_util.py. THis version can run a script properly, but I need to figure out how to conveniently save and load the cluster labels. #2

--- src/pyCapsid/viz_util.py
+++ src/pyCapsid/viz_util.py
@@ -2,7 +2,22 @@
 
 """
 
-def chimeraxLaunchTest(chimerax_path='"C:\\Program Files\\ChimeraX\\bin"', file_path='.' ):
+def chimeraxLaunchTest(labels, chimerax_path='C:\\Program Files\\ChimeraX\\bin', pdb_path='.', save_path='.',
+                       script_path='../src/pyCapsid/scripts/chimerax_script.py', labels_path='.'):
     import os
+    from numpy import save
+    from tempfile import NamedTemporaryFile
+    save(labels_path, labels)
+    print(labels_path)
+
     chimerax_exe = chimerax_path + '\\ChimeraX.exe'
-    os.system('"' + chimerax_exe + '"' + '--script ' + './chimerax_script.py')
+    print(chimerax_exe)
+    cmd_string = f'""{chimerax_exe}" --script "{script_path} {labels_path} {save_path} {pdb_path}""'
+    print(cmd_string)
+    os.system(cmd_string)
+    print('???')
+
+    # results = subprocess.run(['"' + chimerax_exe + '"', '--script',
+    #                           f'"src/pyCapsid/scripts/chimerax_script.py {labels_path} {save_path} {pdb_path}"'],
+    #                          stdout=subprocess.PIPE)
+    # print(results.stdout)
