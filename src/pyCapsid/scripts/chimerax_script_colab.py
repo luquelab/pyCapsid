0ddc7df65202117dc5a654697c6ded9b738887a3
colintravisbrown <54756825+colintravisbrown@users.noreply.github.com>
2023-07-12 16:03:43 -0700

    Fixed pdb identification error in chimerax_script_colab.py #55

--- src/pyCapsid/scripts/chimerax_script_colab.py
+++ src/pyCapsid/scripts/chimerax_script_colab.py
@@ -110,7 +110,7 @@ os.chdir(report_dir)
 
 
 if args['pdb'] is None:
-    fs = [x for x in os.listdir('./') if x.endswith('.npz')]
+    fs = [x for x in os.listdir('./') if x.endswith('_final_results.npz')]
     f = fs[0]
     pdb = f.split('_final')[0]
 else:
