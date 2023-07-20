3224ccd0062b5c5dd95148b8fac53d09023da1ea
colintravisbrown <54756825+colintravisbrown@users.noreply.github.com>
2023-07-20 11:32:36 -0700

    Adding numba version requirement and updating version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 long_description = open("README.md").read()
 setup(
     name='pyCapsid',
-    version='0.5.0',
+    version='0.5.1',
     description='A set of computational tools written in python for the analysis of viral capsids',
     long_description =long_description,
     long_description_content_type='text/markdown',
@@ -23,7 +23,7 @@ setup(
         'scipy',
         'matplotlib',
         'pillow',
-        'numba',
+        'numba>=0.57',
         'statsmodels',
         'toml'
     ],
